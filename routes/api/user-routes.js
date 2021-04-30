1fa56316d331d93435eb1ffc56d8cce0d8c1b9b0
Leah Russell <leahsigridrussell@gmail.com>
2021-04-29 22:47:06 -0700

    Add password hashing functionality and bcrypt

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -16,6 +16,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
   User.findOne({
+    attributes: { exclude: ['password'] },
     where: {
       id: req.params.id
     }
@@ -50,6 +51,7 @@ router.post('/', (req, res) => {
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
