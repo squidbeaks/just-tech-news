1535dfac4c47966f31d8f5b4add6beddecc9cfd3
Leah Russell <leahsigridrussell@gmail.com>
2021-05-06 14:44:16 -0700

    Add post model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User');
+const Post = require("./Post");
 
-module.exports = { User };
+// create associations
+// User.hasMany(post, {
+//   foreignKey: 'user_id'
+// });
+
+module.exports = { User, Post };
