2423b8b116dcb3438db5db30c421c4c63e4cf75e
Leah Russell <leahsigridrussell@gmail.com>
2021-05-06 22:27:47 -0700

    Added get, post, put and delete requests for posts

--- models/index.js
+++ models/index.js
@@ -2,8 +2,12 @@ const User = require('./User');
 const Post = require("./Post");
 
 // create associations
-// User.hasMany(post, {
-//   foreignKey: 'user_id'
-// });
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
 
 module.exports = { User, Post };
